626604b2eea395a5b396a9d69d4b2e1d3da815d1
Glace97 <glacier.ali@gmail.com>
2019-12-04 17:49:27 +0100

    väljpolylinje klar

--- Polylinje/src/PolylinjeTest.java
+++ Polylinje/src/PolylinjeTest.java
@@ -1,41 +1,43 @@
 public class PolylinjeTest {
     public static void main(String[] args) {
 
+        Punkt Ptest1 = new Punkt("F", 1, 2);
+        Punkt[] testLista = new Punkt[]{Ptest1};
+
         //Går att skriva Polylinje1 om man vill testa den andra klassen med samma metoder
-        Polylinje punkt = new Polylinje();
+        //testa konstruktor
+        Polylinje punkt = new Polylinje(testLista);
+
         // 1. (Stack) skapa address/referens punkt som pekar på null dvs oaddresserad
         // 2. (Heap) allokera en ny polylinje och ta addressen dit och stoppa in i punkt
         // new allokerar minne och ger tillbaka addressen dit  MinnesAddress new(Datatyp/Minne)
 
-
-        Polylinje valjPolylinje = new Polylinje();
-
         //Objektet (från polylinje) anropar metoden "laggTill" och skapar ett nytt objekt (från Punkt)
-        //och anger argumentet som objektet Punk (och angiver argument till Punkt i sin tur
         punkt.laggTill(new Punkt("A",3, 4));
         punkt.laggTill(new Punkt ("B", 1, 2));
         punkt.laggTill(new Punkt("C", 2, 3));
         punkt.laggTill(new Punkt("D", 5, 1));
 
         //Första listan av hörn
-        System.out.println(punkt.toString());
+        System.out.println(punkt);
+
+        //testa "ta bort" metoden
+        punkt.taBort("F");
+        System.out.println(punkt);
 
         //metoden lägg till framför lägger punkt C efter punkt B. Tar nya punktobjeketet som argument och namnet på
         //punkten som den kommer komma efter
         punkt.laggTillFramfor(new Punkt("C",2, 3), "B");
-        System.out.println(punkt.toString());
-
-        //testa "ta bort metoden"
-        punkt.taBort("A");
-        System.out.println(punkt.toString());
+        System.out.println(punkt);
 
         //testa färgmetoden
         punkt.setFarg("Gul");
-        System.out.println(punkt.toString());
+        //System.out.println(punkt.toString());
+        System.out.println(punkt);
 
         //testa breddmetoden
         punkt.setBredd(2);
-        System.out.println(punkt.toString());
+        System.out.println(punkt);
 
         //testa avståndsmetoden
         System.out.println(punkt.langd());
@@ -43,5 +45,6 @@ public class PolylinjeTest {
         //testa getters
         System.out.println(punkt.getFarg());
         System.out.println(punkt.getBredd());
+
     }
 }
--- Polylinje/src/ValjPolylinje.java
+++ Polylinje/src/ValjPolylinje.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 class ValjPolylinje {
@@ -9,9 +10,46 @@ class ValjPolylinje {
         Polylinje[] polylinjer = new Polylinje[ANTAL_POLYLINJER];
         for (int i = 0; i < ANTAL_POLYLINJER; i++)
             polylinjer[i] = slumpPolylinje ();
+
         // visa polylinjerna
+        System.out.println("POLYLINJER:" + "\n");
+        for (int i = 0; i < ANTAL_POLYLINJER; i++) {
+            System.out.println(polylinjer[i]);
+            System.out.println("---------------------------------------------------------------------");
+        }
 
         // bestäm den kortaste av de polylinjer som är gula
+        int antalGulaPolylinjer = 0;
+        double[] gulLangdKopia = new double[ANTAL_POLYLINJER];
+        int plats = 0;
+
+        for (int i = 0; i < ANTAL_POLYLINJER; i++) {
+            if (polylinjer[i].getFarg() == "Gul") {
+                antalGulaPolylinjer++;
+
+                gulLangdKopia[plats] = polylinjer[i].langd();
+                plats++;
+            }
+        }
+        if (antalGulaPolylinjer == 0) {          //om inga gula linjer hittas stänger programmet.
+            System.out.println("Inga gula polylinjer hittades.");
+            System.exit(0);
+        }
+        else {
+            double[] gulLangd = new double[antalGulaPolylinjer];
+
+            for (int i = 0; i < antalGulaPolylinjer; i++) {
+                gulLangd[i] = gulLangdKopia[i];                 //samla alla längder i en lista
+            }
+
+            double kortastLangd = gulLangd[0];
+            for (int i = 1; i < antalGulaPolylinjer; i++) {
+                if ( gulLangd[i] < kortastLangd){
+                    kortastLangd = gulLangd[i];
+                }
+            }
+            System.out.println("Den kortaste Gula Polylinjen är: " + kortastLangd + " l.e lång.");
+        }
 
         // visa den valda polylinjen
     }
@@ -19,8 +57,8 @@ class ValjPolylinje {
     // slumpPunkt returnerar en punkt med ett slumpmässigt namn, som är en stor bokstav i
     // det engelska alfabetet, och slumpmässiga koordinater.
     public static Punkt slumpPunkt () {
-        String n = "" + (char) (65 + rand.nextInt (26));   //asciencoded, generera random char
-        int x = rand.nextInt (11);
+        String n = "" + (char) (65 + rand.nextInt (26));   //asciencoded, generera random char. // (datatyp) (range, av typen int, fårn 65 till 26, alla stora bokstäver i AASCi)
+        int x = rand.nextInt (11);     //punkterna väljs slumpmässigt mellan 0-10
         int y = rand.nextInt (11);
         return new Punkt (n, x, y);
     }
@@ -31,19 +69,46 @@ class ValjPolylinje {
     public static Polylinje slumpPolylinje () {
         // skapa en tom polylinje, och lägg till hörn till den
         Polylinje polylinje = new Polylinje ();
-        int antalHorn = 2 + rand.nextInt (7);    //minst 2 antal hörn
-        int antalValdaHorn = 0;
+        int antalHorn = 2 + rand.nextInt (7);    //minst 2 antal hörn, upp till 6. Väljs slumpmässigt fram
+        int antalValdaHorn = 0;           //antal true positioner i valda namn "hur många unika hörn har vi"
         boolean[] valdaNamn = new boolean[26];
 
         // ett och samma namn kan inte förekomma flera gånger
         Punkt valdPunkt = null;
         char valtChar = 0;
 
-        while (antalValdaHorn < antalHorn) {
+        while (antalValdaHorn < antalHorn) {   //
+            valdPunkt = slumpPunkt();      //väljer den slumpade punkten
+            valtChar = valdPunkt.getNamn().charAt(0);   //väljer char som utgör slumpade punktens namn
+
+            //Om en bokstav redan populerat sin plats i listan (satt till true) slumpas en ny punkt fram.
+            while(valdaNamn[valtChar-65] == true) {
+                valdPunkt = slumpPunkt();
+                valtChar = valdPunkt.getNamn().charAt(0);
+            }
+
+            valdaNamn[valtChar-65] = true;  //Den genererade punktens char namn sätter sin index plats i listan till true. Ex C=67, plats 3, index 2
+            polylinje.laggTill(valdPunkt);
+            antalValdaHorn++;              //fram tills vi når antalet punkter i polylinjen körs denna while loop
 
         }
 
         // sätt färg
+        // String[] valbarFarg = new String[]{"Blå", "Röd", "Gul"};
+        int slumpadFarg = rand.nextInt(3); //3 options, kommer genererea 0,1,2
 
+        if (slumpadFarg == 0) {
+            polylinje.setFarg("Blå");
+        }
+
+        if (slumpadFarg == 1) {
+            polylinje.setFarg("Röd");
+        }
+
+        if (slumpadFarg == 2){
+            polylinje.setFarg("Gul");
+        }
+
+        return polylinje;
     }
 }
